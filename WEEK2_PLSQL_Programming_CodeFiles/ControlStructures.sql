f318899c41e0ea30b39b0eae554e4ced7781ae67
gshrutisree <154909230+gshrutisree@users.noreply.github.com>
2025-06-25 10:58:40 +0530

    Mandatory Hands-On Exercise 1

--- WEEK2_PLSQL_Programming_CodeFiles/ControlStructures.sql
+++ WEEK2_PLSQL_Programming_CodeFiles/ControlStructures.sql
@@ -1,28 +1,31 @@
-
-CREATE TABLE Customers ( CustomerID NUMBER PRIMARY KEY, Name VARCHAR2(100), DOB DATE);
-
-CREATE TABLE Loans ( LoanID NUMBER PRIMARY KEY, CustomerID NUMBER, LoanAmount NUMBER, InterestRate NUMBER, StartDate DATE, EndDate DATE, FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID));
-INSERT INTO Customers VALUES (1, 'Ram', TO_DATE('1998-10-15', 'YYYY-MM-DD'));
-INSERT INTO Customers VALUES (2, 'Jayanth', TO_DATE('1980-11-20', 'YYYY-MM-DD'));
-INSERT INTO Customers VALUES (3, 'Sita', TO_DATE('1997-04-01', 'YYYY-MM-DD'));
-INSERT INTO Loans VALUES (1, 1, 5000, 8, TO_DATE('2021-04-01', 'YYYY-MM-DD'), TO_DATE('2025-07-10', 'YYYY-MM-DD'));
-INSERT INTO Loans VALUES (2, 2, 4000, 7, TO_DATE('2021-05-01', 'YYYY-MM-DD'), TO_DATE('2025-07-15', 'YYYY-MM-DD'));
-INSERT INTO Loans VALUES (3, 3, 6000, 9, TO_DATE('2021-06-01', 'YYYY-MM-DD'), TO_DATE('2025-07-20', 'YYYY-MM-DD'));
-COMMIT;
-BEGIN
-FOR rec IN (
-    SELECT l.LoanID, c.DOB
-    FROM Customers c
-    JOIN Loans l ON c.CustomerID = l.CustomerID
-) 
-LOOP
-    IF FLOOR(MONTHS_BETWEEN(SYSDATE, rec.DOB) / 12) > 60 THEN
-        UPDATE Loans
-        SET InterestRate = InterestRate - 1
-        WHERE LoanID = rec.LoanID;
-    END IF;
-END LOOP;
-COMMIT;
-END;
+create table customers (
+  customerid number primary key,
+  name varchar2(100),
+  dob date,
+  balance number,
+  lastmodified date
+);
+create table loans (loanid number primary key,customerid number,loanamount number,interestrate number,startdate date,enddate date,foreign key (customerid) references customers(customerid));
+insert into customers values (1, 'John Doe', to_date('1955-05-15', 'yyyy-mm-dd'), 1000, sysdate);
+insert into customers values (2, 'Jane Smith', to_date('1990-07-20', 'yyyy-mm-dd'), 1500, sysdate);
+insert into customers values (3, 'Kiran Rao', to_date('1940-01-01', 'yyyy-mm-dd'), 2000, sysdate);
+insert into loans values (1, 1, 5000, 5, sysdate, add_months(sysdate, 60));
+insert into loans values (2, 2, 7000, 6, sysdate, add_months(sysdate, 60));
+insert into loans values (3, 3, 6000, 8, sysdate, add_months(sysdate, 60));
+commit;
+begin
+  for rec in (
+    select l.loanid, c.dob
+    from customers c
+    join loans l on c.customerid = l.customerid
+  ) loop
+    if floor(months_between(sysdate, rec.dob) / 12) > 60 then
+      update loans
+      set interestrate = interestrate - 1
+      where loanid = rec.loanid;
+    end if;
+  end loop;
+  commit;
+end;
 /
-SELECT * FROM Loans;
+select * from loans;
