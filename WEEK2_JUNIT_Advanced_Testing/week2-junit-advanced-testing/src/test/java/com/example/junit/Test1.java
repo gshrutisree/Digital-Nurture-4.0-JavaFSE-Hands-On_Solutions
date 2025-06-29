b20615d332e2d1921ec522ad603c0d730185dbb8
gshrutisree <154909230+gshrutisree@users.noreply.github.com>
2025-06-29 18:54:48 +0530

    Exercise 2

--- WEEK2_JUNIT_Advanced_Testing/week2-junit-advanced-testing/src/test/java/com/example/junit/Test1.java
+++ WEEK2_JUNIT_Advanced_Testing/week2-junit-advanced-testing/src/test/java/com/example/junit/Test1.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class Test1 {
-
+ 
     @Test
     void testAddition() {
         assertEquals(5, 2 + 3, "2 + 3 should be 5");
