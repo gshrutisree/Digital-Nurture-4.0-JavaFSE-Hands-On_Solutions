77d6fd85660433d6e0dbc4622bdd44864a761e60
gshrutisree <154909230+gshrutisree@users.noreply.github.com>
2025-06-29 20:12:24 +0530

    Exercise 3

--- WEEK2_Advanced_Mockito_CodeFiles/week2-mockito-advanced/src/main/java/com/example/mockitoadv/FileService.java
+++ WEEK2_Advanced_Mockito_CodeFiles/week2-mockito-advanced/src/main/java/com/example/mockitoadv/FileService.java
@@ -1,7 +1,7 @@
 package com.example.mockitoadv;
 
 public class FileService {
-    private final FileReader fileReader;
+    private final FileReader fileReader; 
     private final FileWriter fileWriter;
 
     public FileService(FileReader fileReader, FileWriter fileWriter) {
