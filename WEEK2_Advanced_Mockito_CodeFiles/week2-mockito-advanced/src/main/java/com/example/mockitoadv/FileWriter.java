9d6627a1baff88d94ac11b87811ba95efd4bbf32
Shruti Gullapalli <gshruti@Shrutis-MacBook-Air.local>
2025-06-29 20:09:13 +0530

    Added Week 2 Advanced Mockito code files and test cases

---
+++ WEEK2_Advanced_Mockito_CodeFiles/week2-mockito-advanced/src/main/java/com/example/mockitoadv/FileWriter.java
@@ -0,0 +1,5 @@
+package com.example.mockitoadv;
+
+public interface FileWriter {
+    void write(String content);
+}
---
+++ WEEK2_Advanced_Mockito_CodeFiles/week2-mockito-advanced/src/main/java/com/example/mockitoadv/NetworkClient.java
@@ -0,0 +1,5 @@
+package com.example.mockitoadv;
+
+public interface NetworkClient {
+    String connect();
+}
