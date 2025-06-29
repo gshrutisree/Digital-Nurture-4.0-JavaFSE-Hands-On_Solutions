826f9fec19e10729843dbdc9e3cfc5d32b128e00
gshrutisree <154909230+gshrutisree@users.noreply.github.com>
2025-06-29 18:50:44 +0530

    Exercise 1

--- WEEK2_JUNIT_Advanced_Testing/week2-junit-advanced-testing/src/test/java/com/example/junit/EvenCheckerTest.java
+++ WEEK2_JUNIT_Advanced_Testing/week2-junit-advanced-testing/src/test/java/com/example/junit/EvenCheckerTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class EvenCheckerTest {
-
+ 
     EvenChecker c = new EvenChecker();
 
     @ParameterizedTest
