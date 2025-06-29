eb8131a82e6b956ce74b83ac07b2e3aebe8eab8a
gshrutisree <154909230+gshrutisree@users.noreply.github.com>
2025-06-29 18:53:53 +0530

    Exercise 3

--- WEEK2_JUNIT_Advanced_Testing/week2-junit-advanced-testing/src/test/java/com/example/junit/OrderedTests.java
+++ WEEK2_JUNIT_Advanced_Testing/week2-junit-advanced-testing/src/test/java/com/example/junit/OrderedTests.java
@@ -1,5 +1,4 @@
 package com.example.junit;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
