43fbcacee506fb341a07e06de7d2725d97e0fdbe
gshrutisree <154909230+gshrutisree@users.noreply.github.com>
2025-06-29 20:14:14 +0530

    Exercise 4

--- WEEK2_Advanced_Mockito_CodeFiles/week2-mockito-advanced/src/test/java/com/example/mockitoadv/NetworkServiceTest.java
+++ WEEK2_Advanced_Mockito_CodeFiles/week2-mockito-advanced/src/test/java/com/example/mockitoadv/NetworkServiceTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class NetworkServiceTest {
+public class NetworkServiceTest { 
 
     @Test
     public void testServiceWithMockNetworkClient() {
